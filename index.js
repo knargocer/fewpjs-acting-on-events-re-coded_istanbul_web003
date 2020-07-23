72e1d8b8d7131e0a0501ee33f061c23a9e60198c
Knar Gocer <knargr@gmail.com>
2020-07-23 14:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
  
   if (right > left) {
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 }
